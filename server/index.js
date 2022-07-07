e40fe8954f0b0560a887cee282e4f74277b7d603
Kevin Ha <kevincw.ha@gmail.com>
2022-07-07 16:37:44 -0700

    server test

--- server/index.js
+++ server/index.js
@@ -2,11 +2,40 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+// import api files
+const overview = require('./OverviewAPI.js');
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // TODO: Define routes
 
-app.listen(PORT);
+//OVERVIEW - KEVIN
+// app.get('/product', (req, res) => {
+//   overview.listProducts(req.params/req.query)
+//   res.send
+
+// })
+
+// PRODUCTS
+
+app.get('/product', overview.listProducts)
+// app.get('/product/id', overview.productInfo)
+// app.get('/product/styles', overview.productStyles)
+
+// REVIEWS
+
+
+// QUESTIONS & ANSWERS
+
+
+// CART
+
+// RELATED ITEMS
+app.get('/product', overview.relatedProducts)
+
+
+// INTERACTIONS
+
+app.listen(port);
 console.log(`Server listening at http://localhost:${port}`);
