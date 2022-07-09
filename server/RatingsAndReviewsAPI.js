f790e1d7675900f1c0bef6feb318eb350e9e7f73
chrisxchoi <chris@choi.is>
2022-07-08 17:32:17 -0700

    feat/chore: Implement server controllers and install styled components

--- server/RatingsAndReviewsAPI.js
+++ server/RatingsAndReviewsAPI.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 const config = require('../config.js');
 
+let options = {
+  headers: {
+    'Authorization': config.TOKEN
+  },
+ }
+
 exports.listReviews = (req, res) => {
   console.log(req.query)
   let options = {
@@ -46,22 +52,6 @@ exports.getReviewMetadata = (req, res) => {
 }
 
 exports.addReview = (req, res) => {
-  let options = {
-    headers: {
-      'Authorization': config.TOKEN
-    },
-    // params: {
-    //   product_id: req.params.product_id,
-    //   rating: req.params.rating,
-    //   summary: req.params.summary,
-    //   body: req.params.body,
-    //   recommend: req.params.recommend,
-    //   name: req.params.name,
-    //   email: req.params.email,
-    //   photos: req.params.photos,
-    //   characteristics: req.params.characteristics,
-    // }
-   }
    let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/`;
    axios.post(url, req.body, options)
       .then( (result) => res.status(200).send(result.data) )
@@ -72,11 +62,6 @@ exports.markAsHelpful = (req, res) => {
   // 1274559
   console.log('REQUEST IS...' ,req)
   console.log('REQUEST PARAMS IS...' ,req.params)
-  let options = {
-    headers: {
-      'Authorization': config.TOKEN
-    }
-  }
   let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/${req.params.review_id}/helpful`;
   axios.put(url, req.params, options)
       .then( (result) => res.status(200).send(result.data))
@@ -84,11 +69,6 @@ exports.markAsHelpful = (req, res) => {
 }
 
 exports.reportReview = (req, res) => {
-  let options = {
-    headers: {
-      'Authorization': config.TOKEN
-    }
-  }
   axios.put(`/reviews/${req.params.review_id}/report`, options)
       .then( (result) => res.status(200).send(result.data))
       .catch( (e) => res.status(404).send(e))
