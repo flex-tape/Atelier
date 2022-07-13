689d102ea62ff527cbc94dd5de9f6c03760f9759
Toan Dao <toanddao@gmail.com>
2022-07-12 17:43:37 -0700

    feat/fix: fetch product info and styles for related products. render data onto component

--- client/src/components/RelatedItems/RelatedCard.jsx
+++ client/src/components/RelatedItems/RelatedCard.jsx
@@ -1,25 +1,62 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const PrimaryImage = styled.img`
+height: 350px;
+width: 350px;
+object-fit: contain;
+`
+
+const RelatedItemsList = styled.div`
+display: inline-block;
+`
 
 export default function RelatedCard({item}) {
   const [relatedProductInfo, setRelatedProductInfo] = useState([]);
 
-  let productInfo =  axios.get(`/products/${item}`)
-  .then((res) => {
-    console.log('product level info: ', res.data);
-    return res.data;
-  })
+  const getRelatedInfo = async () => {
+    let productLevelInfo = await axios.get(`/products/${item}`)
+      .then((res) => {
+      console.log('product level info: ', res.data);
+      return res.data;
+    })
 
-  let productStyles = axios.get(`/products/${item}/styles`)
-  .then((res) => {
-    console.log('product styles: ', res.data);
-    return res.data;
-  })
+    let productStyles = await axios.get(`/products/${item}/styles`)
+      .then((res) => {
+      // console.log('product styles: ', res.data);
+      return res.data;
+    })
+
+
+
+
+    let primaryPhoto = '';
+    if (productStyles.results[0].photos[0].url === null) {
+      primaryPhoto = 'https://images.unsplash.com/photo-1553830591-2f39e38a013c?ixlib=rb-1.2.1&auto=format&fit=crop&w=2760&q=80';
+    } else {
+      primaryPhoto = productStyles.results[0].photos[0].url;
+    }
+
+    let relatedInfo = {
+      name: productLevelInfo.name,
+      category: productLevelInfo.category,
+      price: productStyles.results[0].original_price,
+      image: primaryPhoto
+    }
+
+    await setRelatedProductInfo(relatedInfo)
+  }
+
+  useEffect(() => {
+    getRelatedInfo();
+  }, [])
+
 
   return (
-    <div>
-      <p>{item}</p>
-    </div>
+    <RelatedItemsList>
+      <PrimaryImage src={relatedProductInfo.image}></PrimaryImage>
+    </RelatedItemsList>
   )
 }
---
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable no-unused-vars */
+import React, { useState, useContext, useEffect } from 'react';
+import { IDContext } from '../App.jsx';
+import axios from 'axios';
+import RelatedCard from './RelatedCard.jsx'
+
+
+export default function RelatedItems({ setID }) {
+  // will need product ID of current product
+  // grab related products of current produt
+  // related products for 40344: 40345, 40346, 40351, 40350
+  const [relatedItems, setRelatedItems] = useState([40345, 40346, 40351, 40350]);
+
+  let productID = useContext(IDContext)
+
+  const getRelatedItems = () => {
+    axios.get('/productsrelated', {params: {product_id: productID}})
+    .then((res) => {
+      setRelatedItems(res.data);
+    })
+  }
+
+  // run getRelatedItems whenever selected productID changes
+  useEffect(() => {
+    getRelatedItems
+  }, [productID]);
+
+
+  return (
+    <div>
+      <div>
+        <h3>Related Products</h3>
+          <div>
+            {relatedItems.map((item, index) => (
+              <RelatedCard item={item} key={index}/>
+            ))}
+          </div>
+        <button onClick={getRelatedItems}>Get Related</button>
+
+      </div>
+      <div>
+        <h3>Your Outfit</h3>
+
+
+      </div>
+    </div>
+  )
+}
