fd16d2e44b4d3c6967d0d3233e611a698b602aab
Toan Dao <toanddao@gmail.com>
2022-07-14 11:35:35 -0700

    Fix merge changes

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -118,18 +118,3 @@ export default function Overview (props) {
   )
 }
 
-  // const changeID = () => {
-  //   props.setProductID(65656);
-  // }
-
-<<<<<<< HEAD
-=======
-  return (
-    <div>
-      <h1>HELLO, OVERVIEW!</h1>
-      <button onClick={()=> props.setProductID(props.productID + 1)}>Click here!</button>
-      {props.productID}
-    </div>
-  )
-}
->>>>>>> main
