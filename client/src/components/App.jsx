5811b27e4457298fa01f8009a1d729cebaaac994
chrisxchoi <chris@choi.is>
2022-07-13 22:27:50 -0700

    Revert "Push protection test". Push protection failed.
    This reverts commit b74ef3a7907e0edf7b6f8b3d62c12d2ad23b0537.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   // const [productStyles, setProductStyle] = useState([]); // might not need this here, because to get product styles, all you need is product_id
   //
   // pass setProductID as prop to related items
-  console.log('test')
+
   return (
     <div>
       <h1>HELLO, WORLD!</h1>
