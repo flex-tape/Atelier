9f5e800d691f148a20b971234e3cccc212c7eff8
Toan Dao <toanddao@gmail.com>
2022-07-14 11:19:25 -0700

    Delete extra productInfo function

--- server/OverviewAPI.js
+++ server/OverviewAPI.js
@@ -25,7 +25,7 @@ exports.listProducts = (req, res) => {
 }
 
 exports.productInfo = (req, res) => {
-  console.log(req);
+  console.log(req.params);
   const product_id = req.params.product_id;
     let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${product_id}`;
     let options = {
@@ -43,29 +43,29 @@ exports.productInfo = (req, res) => {
     })
 }
 
-exports.productInfo = (req, res) => {
-  console.log('info req', req)
-  // /products/id/product_id
-  // /products/asda231a
+// exports.productInfo = (req, res) => {
+//   console.log('info req', req)
+//   // /products/id/product_id
+//   // /products/asda231a
 
-  // req.params = :id
-  let product_id = `${req.params.product_id}`; // req.body should be req.query or req.params
-  let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${product_id}`
-  let options = {
-    headers: {
-      'Authorization': config.TOKEN
-    }
-  }
-  axios.get(url, options)
-  .then((response) => {
-    // console.log('resp', response)
-    res.status(200).send(response)
-  })
-  .catch((err) => {
-    console.log('err', err);
-    res.status(400).send(err);
-  })
-}
+//   // req.params = :id
+//   let product_id = `${req.params.product_id}`; // req.body should be req.query or req.params
+//   let url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${product_id}`
+//   let options = {
+//     headers: {
+//       'Authorization': config.TOKEN
+//     }
+//   }
+//   axios.get(url, options)
+//   .then((response) => {
+//     // console.log('resp', response)
+//     res.status(200).send(response)
+//   })
+//   .catch((err) => {
+//     console.log('err', err);
+//     res.status(400).send(err);
+//   })
+// }
 
 exports.productStyles = (req, res) => {
   console.log(req.query.product_id)
