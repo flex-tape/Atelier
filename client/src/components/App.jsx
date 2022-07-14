6c486cd3bbb0fb6ac779ab1a51d12214e0fcd489
Toan Dao <toanddao@gmail.com>
2022-07-14 10:29:32 -0700

    Add Main changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,37 +1,13 @@
-<<<<<<< HEAD
-import React from 'react';
-// import ReactDOM from 'react-dom';
-// import Overview from './Overview.jsx';
-import QandA from './QandA/QandA.jsx';
-// import RatingsAndReviews from './RatingsAndReviews.jsx';
-// import RelatedItems from './RelatedItems.jsx';
-=======
 import React, { useState } from 'react';
 // import ReactDOM from 'react-dom';
 import Overview from './Overview/Overview.jsx';
 // import QandA from './QandA/QandA.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
 // import RelatedItems from './RelatedItems/RelatedItems.jsx';
->>>>>>> main
 
 export default function App() {
   const [productID, setProductID] = useState(40344);
 
-<<<<<<< HEAD
-    }
-  }
-  render () {
-    return (
-      <div>
-        <h1>FEC</h1>
-        {/* <Overview/> */}
-        <QandA />
-        {/* <RatingsAndReviews/> */}
-        {/* <RelatedItems/> */}
-      </div>
-    )
-  }
-=======
   // const [productID, setProductID] = useState(() => { return 40344; }); best practice but probably doesn't matter here
   // const [productStyles, setProductStyle] = useState([]); // might not need this here, because to get product styles, all you need is product_id
   //
@@ -45,5 +21,4 @@ export default function App() {
       {/* <RelatedItems handleRelatedItemClick={setProductID} productID={productID}/> */}
     </div>
   )
->>>>>>> main
 }
