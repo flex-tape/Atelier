e25d0d0a988e552e1b87b1ed93a3991ecb168181
Kevin Ha <kevincw.ha@gmail.com>
2022-07-14 11:14:20 -0700

    merged with main

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,8 +3,4 @@ import {createRoot} from 'react-dom/client';
 import App from './components/App.jsx';
 
 const root = createRoot(document.getElementById('app'));
-<<<<<<< HEAD
 root.render(<App />);
-=======
-root.render(<App />);
->>>>>>> main
